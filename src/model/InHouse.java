2588c699964dbb279cd93952ea6a2c61f737e98f
jveps <33668417+jveps@users.noreply.github.com>
2021-02-16 00:58:18 -0800

    Update InHouse.java

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -8,7 +8,7 @@ package model;
 /**
  * This class creates InHouse parts.
  * This class creates and modifies parts and their attributes.
- * @author Jessie Van Epps
+ * @author jveps
  */
 public class InHouse extends Part {
     
